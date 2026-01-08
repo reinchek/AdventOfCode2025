e01641fea770d87bcc263300860ad1e17aca36ad
Nino Marrazzo <nino.marrazzo@hintogroup.eu>
2026-01-08 16:53:51 +0100

    Day6: finally resolved.

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -2,18 +2,11 @@
 // Solve the problems on the math worksheet. What is the grand total found by adding together all of the answers to the individual problems?
 
 use std::arch::x86_64::_mulx_u32;
-use std::fs::read_to_string;
+use std::fs::{read_to_string, DirBuilder};
 use aoc2025::read_input;
 
 fn main() {
-    //let input = read_input(6);
-    let input =
-"
-123 328  51 64
- 45 64  387 23
-  6 98  215 314
-*   +   *   +
-";
+    let input = read_input(6, Some(false));
 
     // ██████╗  █████╗ ██████╗ ████████╗     ██╗
     // ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ███║
@@ -22,6 +15,7 @@ fn main() {
     // ██║     ██║  ██║██║  ██║   ██║        ██║
     // ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝        ╚═╝
     let operations: Vec<char> = input
+        .trim()
         .lines()
         .last()
         .unwrap()
@@ -60,8 +54,6 @@ fn main() {
         }
         matrix_cols_rows_swapped.push(swapped_row);
     }
-    println!("original_matrix: {:?}", original_matrix);
-    println!("matrix_cols_rows_swapped: {matrix_cols_rows_swapped:?}");
 
     let mut grand_total: i128 = 0;
     for (row_index, row) in matrix_cols_rows_swapped.iter().enumerate() {
@@ -79,13 +71,7 @@ fn main() {
     // ██╔═══╝ ██╔══██║██╔══██╗   ██║       ██╔═══╝
     // ██║     ██║  ██║██║  ██║   ██║       ███████╗
     // ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚══════╝
-    let mut rtl_columns: Vec<Vec<isize>> = Vec::new();
 
-    // for row in matrix_cols_rows_swapped {
-    //     for col in row {
-    //         let num2str = col.to_string().chars().rev().collect::<String>().parse::<i128>().unwrap();
-    //     }
-    // }
     enum Operations {
         Add,
         Mul
@@ -100,19 +86,14 @@ fn main() {
         }
     }
 
-    struct Numbers {
-        numbers: Vec<Vec<i128>>,
-        operation: Operations,
-    }
 
-
-    // |1|2|3|3|2|8|5|1| |6|4| |
-    // |4|5| |6|4| |3|8|7|2|3| |
-    // |6| | |9|8| |2|1|5|3|1|4|
-    // |*| | |+| | |*| | |+| | |
     let mut last_signs_row = input.lines().last().unwrap();
+    let signs_splitted_whitespaces = last_signs_row.split(['*', '+']);
     let signs_splitted = last_signs_row.split_inclusive(['*', '+']);
-    let signs_splitted_array: Vec<&str> = signs_splitted.clone().collect();
+    let mut signs_splitted_array_whitespaces: Vec<&str> = signs_splitted_whitespaces.clone().collect();
+    signs_splitted_array_whitespaces.remove(0);
+
+
     let input_lines = input
         .trim()
         .lines()
@@ -122,89 +103,55 @@ fn main() {
     let mut grand_total = 0;
 
     let mut greatest_number_len: usize = 0;
-    for (sign_index, sign_str) in signs_splitted.enumerate() {
-        if sign_index == 0 {
-            greatest_number_len = signs_splitted_array[sign_index + 1].len() - 1;
+    let mut col_offset = 0;
+    let mut numbers: Vec<i128> = Vec::new();
+    let input_lines: Vec<&str> = input_lines[0..input_lines.len()-1].to_vec();
+
+    for (sign_index, sign_str) in signs_splitted.map(|sign| sign.trim()).enumerate() {
+        if sign_index == signs_splitted_array_whitespaces.len()-1 {
+            greatest_number_len = &input_lines.last().unwrap().len() - last_signs_row.len() + 1;
         } else {
-            greatest_number_len = sign_str.len() - 1;
+            greatest_number_len = signs_splitted_array_whitespaces[sign_index].len();
         }
-    }
 
-    // |1|2|3| |3|2|8|5|1| |6|4| | <-- len(): 11
-    // |4|5| | |6|4| |3|8|7|2|3| |
-    // |6| | | |9|8| |2|1|5|3|1|4|
-    // |*| | | |+| | |*| | |+| | |
-    let mut curr_pos = 0;
-    let mut col_numbers: Vec<i128> = Vec::new();
 
-    while curr_pos < input_lines[0].len()-1 {
+        let mut temp_number: String = "".into();
+        let sign = sign_str.trim();
 
-        let mut temporary_number: Vec<i128> = Vec::new();
-    
-        dbg!(curr_pos);
-        for i in curr_pos..(curr_pos+greatest_number_len) {
-            let mut local_number: String = String::new();
-
-            for row in &input_lines {
-                let row = row.chars().collect::<Vec<char>>();
-                if row[i] != ' ' {
-
-                    local_number.push(row[i]);
+        for col in col_offset..col_offset+greatest_number_len {
+            for line in &input_lines {
+                if col == line.len() {
+                    continue;
+                }
+                let char = &line[col..=col];
+                if !char.is_empty() && char != " " {
+                    temp_number.push(line[col..=col].chars().collect::<Vec<char>>()[0]);
                 }
             }
-            if !local_number.is_empty() {
-                temporary_number.push(local_number.parse::<i128>().unwrap());
+            if temp_number.is_empty() {
+                temp_number = match Operations::from(sign) {
+                    Operations::Mul => "1".to_string(),
+                    Operations::Add => "0".to_string()
+                };
+            }
+
+            numbers.push(temp_number.parse::<i128>().unwrap());
+
+            temp_number.clear();
+
+            if numbers.len() == greatest_number_len {
+                grand_total += match Operations::from(sign) {
+                    Operations::Mul => numbers.iter().product::<i128>(),
+                    Operations::Add => numbers.iter().sum::<i128>()
+                };
+
+                numbers.clear();
+
+                col_offset += greatest_number_len+1;
             }
         }
-
-        println!("curr_pos: {curr_pos}");
-        println!("col_numbers: {temporary_number:?}");
-        if last_signs_row.chars().nth(curr_pos).unwrap() == '*' {
-            println!("mul;");
-            grand_total += temporary_number.iter().product::<i128>();
-        } else {
-            println!("add;");
-
-            grand_total += temporary_number.iter().sum::<i128>();
-        }
-
-        curr_pos += greatest_number_len+1;
     }
-    dbg!(grand_total);
-    //
-    //
-    // // 123 328  51   64
-    // // 45  64   387  23
-    // // 6   98   215  314
-    // // *   +   *   +
-    //
-    // // 123 45  6
-    // // 328 64  98
-    // // 51  387 215
-    // // 64  23  314    [6][4][]|[2][3][]|[3][1][4]
-    //
-    // let mut matrix_of_cells: Vec<Vec<Vec<i128>>> = vec!();
-    //
-    // for row in matrix_cols_rows_swapped {
-    //     let mut row_with_cells_digits: Vec<Vec<i128>> = vec!();
-    //
-    //     for col in row {
-    //         let mut num_to_cell: Vec<i128> = vec!();
-    //         // ex: 64  => vec![6, 4]
-    //         //     23  => vec![2, 3]
-    //         //     314 => vec![3, 1, 4]
-    //         for digit in col.to_string().chars() {
-    //             let converted_digit = digit.clone().to_digit(10).unwrap() as i128;
-    //             &num_to_cell.push(converted_digit);
-    //         }
-    //         row_with_cells_digits.push(num_to_cell);
-    //     }
-    //     println!("row => {:?}", row_with_cells_digits);
-    //     matrix_of_cells.push(row_with_cells_digits);
-    // }
-    // println!("full => {:?}", matrix_of_cells);
-    //
-    // // println!("rtl_columns -> {rtl_columns:?}");
 
+    println!("#Part 2) What is the grand total found by adding together all of the answers to the individual problems? {grand_total}");
 }
 
